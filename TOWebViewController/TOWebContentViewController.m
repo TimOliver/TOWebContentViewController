96f7cd6502e590adc6d53af7ac8bee9a92bb61b5
Tim Oliver <me@timoliver.com.au>
2018-10-14 23:58:47 +0900

    Added HTML files

--- TOWebViewController/TOWebContentViewController.m
+++ TOWebViewController/TOWebContentViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "TOWebContentViewController.h"
+#import "_TOWebContentView.h"
 
 @interface TOWebContentViewController ()
+
+@property (nonatomic, readwrite) WKWebView *webView;
 
 @end
 
@@ -16,17 +19,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    
+    
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
+
--- TOWebViewController/_TOWebContentView.h
+++ TOWebViewController/_TOWebContentView.h
@@ -1,19 +1,16 @@
 //
-//  TOWebContentViewController.h
+//  _TOWebContentView.h
 //  TOWebContentViewControllerExample
 //
 //  Created by Tim Oliver on 14/10/18.
 //  Copyright © 2018 Tim Oliver. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
 #import <WebKit/WebKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface TOWebContentViewController : UIViewController
-
-@property (nonatomic, readonly) WKWebView *webView;
+@interface _TOWebContentView : WKWebView
 
 @end
 
